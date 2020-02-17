9b4e17236124460535982232bbb3ed16b8cacbb5
Gerald <geraldmir4@gmail.com>
2020-02-18 00:54:37 +0800

    Remove unused row

--- lib/tabviews/control.dart
+++ lib/tabviews/control.dart
@@ -12,16 +12,7 @@ class Control extends StatelessWidget {
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
           ControlSliders(),
-          Row(
-            children: <Widget>[
-              const Text("Feed"),
-              RaisedButton(
-                onPressed: manualFeed(context),
-                child: Text("Do it"),
-              ),
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-          ),
+          Divider(),
           NumberInput(),
         ],
       ),
